1e0ad35255b08619b350fd287227391c0482de8b
Hendi Santika <hendisantika@gmail.com>
2017-11-16 06:46:57 +0700

    #2 - Add jpa & mysql libs

---
+++ src/main/java/com/hendisantika/springbootorders/model/Customer.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springbootorders.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-orders
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/11/17
+ * Time: 06.43
+ * To change this template use File | Settings | File Templates.
+ */
+public class Customer {
+}
