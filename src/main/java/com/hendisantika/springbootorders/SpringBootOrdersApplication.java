6c59d55139c1da3f5d73c7bb14cfdc557570ddba
Hendi Santika <hendisantika@gmail.com>
2017-11-16 06:42:23 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootorders/SpringBootOrdersApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOrdersApplication.class, args);
+	}
+}
