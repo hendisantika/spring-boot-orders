796d0a0be6c0c34ae89a5b41a3ee35b77a1ae2a9
Hendi Santika <hendisantika@gmail.com>
2017-11-16 06:47:15 +0700

    #3 - Add Customer model

--- src/main/java/com/hendisantika/springbootorders/model/Customer.java
+++ src/main/java/com/hendisantika/springbootorders/model/Customer.java
@@ -1,5 +1,7 @@
 package com.hendisantika.springbootorders.model;
 
+import javax.persistence.*;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-orders
@@ -10,5 +12,89 @@ package com.hendisantika.springbootorders.model;
  * Time: 06.43
  * To change this template use File | Settings | File Templates.
  */
+
+@Entity
 public class Customer {
+    @Id
+    @Column(name = "customerId")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int customerId;
+    String cusFirstname;
+    String cusLastname;
+    String cusEmail;
+    String cusPhoneNo;
+    String cusCity;
+    String cusProvince;
+    String cusCountry;
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCusFirstname() {
+        return cusFirstname;
+    }
+
+    public void setCusFirstname(String cusFirstname) {
+        this.cusFirstname = cusFirstname;
+    }
+
+    public String getCusLastname() {
+        return cusLastname;
+    }
+
+    public void setCusLastname(String cusLastname) {
+        this.cusLastname = cusLastname;
+    }
+
+    public String getCusEmail() {
+        return cusEmail;
+    }
+
+    public void setCusEmail(String cusEmail) {
+        this.cusEmail = cusEmail;
+    }
+
+    public String getCusPhoneNo() {
+        return cusPhoneNo;
+    }
+
+    public void setCusPhoneNo(String cusPhoneNo) {
+        this.cusPhoneNo = cusPhoneNo;
+    }
+
+    public String getCusCity() {
+        return cusCity;
+    }
+
+    public void setCusCity(String cusCity) {
+        this.cusCity = cusCity;
+    }
+
+    public String getCusProvince() {
+        return cusProvince;
+    }
+
+    public void setCusProvince(String cusProvince) {
+        this.cusProvince = cusProvince;
+    }
+
+    public String getCusCountry() {
+        return cusCountry;
+    }
+
+    public void setCusCountry(String cusCountry) {
+        this.cusCountry = cusCountry;
+    }
+
+    @Override
+    public String toString() {
+        return "customerId = " + customerId + " , cusFirstname = " + cusFirstname + " , cusLastname = " + cusLastname +
+                " , cusEmail = " + cusEmail + " , cusPhoneNo = " + cusPhoneNo + " , cusCity = " + cusCity +
+                ", cusProvince = " + cusProvince + " , cusCountry = " + cusCountry;
+    }
 }
