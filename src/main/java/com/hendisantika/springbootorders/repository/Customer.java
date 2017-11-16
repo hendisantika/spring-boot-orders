3d1baf3e7f3eb05ec54fab998a6003143af9dd4e
Hendi Santika <hendisantika@gmail.com>
2017-11-16 07:13:13 +0700

    #9 - Add Customer repository

---
+++ src/main/java/com/hendisantika/springbootorders/repository/Customer.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springbootorders.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-orders
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/11/17
+ * Time: 07.12
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Customer extends CrudRepository {
+}
