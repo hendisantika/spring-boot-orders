53ed8b775ff84d309429c091fee5681784ac0a07
Hendi Santika <hendisantika@gmail.com>
2022-04-24 12:07:08 +0700

    #94 - Add list orders OrderWebController

--- src/main/java/com/hendisantika/springbootorders/controller/OrderWebController.java
+++ src/main/java/com/hendisantika/springbootorders/controller/OrderWebController.java
@@ -1,10 +1,15 @@
 package com.hendisantika.springbootorders.controller;
 
+import com.hendisantika.springbootorders.model.Orders;
 import com.hendisantika.springbootorders.repository.OrderRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +26,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OrderWebController {
     private final OrderRepo orderRepo;
+
+    @GetMapping("/list")
+    public String listOrders(Model model) {
+        List<Orders> orders = orderRepo.findAll();
+        model.addAttribute("orders", orders);
+        return "order/order_list";
+    }
 }
