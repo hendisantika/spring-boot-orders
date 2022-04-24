720c668614a9e02e76ba2b9c84bbe3198112d887
Hendi Santika <hendisantika@gmail.com>
2022-04-24 09:25:22 +0700

    #76 - Enhance controller

--- src/main/java/com/hendisantika/springbootorders/controller/CustomerController.java
+++ src/main/java/com/hendisantika/springbootorders/controller/CustomerController.java
@@ -2,10 +2,14 @@ package com.hendisantika.springbootorders.controller;
 
 import com.hendisantika.springbootorders.model.Customer;
 import com.hendisantika.springbootorders.repository.CustomerRepo;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -21,20 +25,19 @@ import java.util.List;
  */
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/customers")
 public class CustomerController {
-    @Autowired
-    CustomerRepo customerRepo;
-
+    private final CustomerRepo customerRepo;
 
     @GetMapping
     ResponseEntity<List<Customer>> getCustomers(){
-        return new ResponseEntity<List<Customer>>((List<Customer>) customerRepo.findAll(), HttpStatus.OK);
+        return new ResponseEntity<>((List<Customer>) customerRepo.findAll(), HttpStatus.OK);
     }
 
     @PostMapping
     ResponseEntity<Customer> saveCustomer(@RequestBody Customer customer){
-        return new ResponseEntity<Customer>(customerRepo.save(customer), HttpStatus.OK);
+        return new ResponseEntity<>(customerRepo.save(customer), HttpStatus.OK);
     }
 
 
--- src/main/java/com/hendisantika/springbootorders/controller/OrderController.java
+++ src/main/java/com/hendisantika/springbootorders/controller/OrderController.java
@@ -2,10 +2,14 @@ package com.hendisantika.springbootorders.controller;
 
 import com.hendisantika.springbootorders.model.Orders;
 import com.hendisantika.springbootorders.repository.OrderRepo;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -22,17 +26,17 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/orders")
+@RequiredArgsConstructor
 public class OrderController {
-    @Autowired
-    OrderRepo orderRepo;
+    private final OrderRepo orderRepo;
 
     @GetMapping
     ResponseEntity<List<Orders>> getOrders(){
-        return new ResponseEntity<List<Orders>>((List<Orders>) orderRepo.findAll(), HttpStatus.OK);
+        return new ResponseEntity<>(orderRepo.findAll(), HttpStatus.OK);
     }
 
     @PostMapping
     ResponseEntity<Orders> saveOrder(@RequestBody Orders orders){
-        return new ResponseEntity<Orders>(orderRepo.save(orders), HttpStatus.OK);
+        return new ResponseEntity<>(orderRepo.save(orders), HttpStatus.OK);
     }
 }
